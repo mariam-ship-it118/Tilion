769443e8e4de6b3fb89210fcf9a8d7f9dd49d338
mariam-ship-it118 <22-101170@students.eui.edu.eg>
2026-04-03 05:44:19 +0200

    blog page fixed completed

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import BlogsTitleComponent from '../Components/BlogsMain';
 import NavBar from '../Components/NavBar';
 import HoverCard from '../Components/EventsCards';
-import Blog1 from "../Assets/blog1.svg"
+import Blog1 from "../Assets/blog1.svg";
+import Blog2 from "../Assets/blog2.svg";
+import Blog3 from "../Assets/blog3.svg";
 import Footer from '../Components/Footer';
-
 const Blogs = () => {
     return ( <>
     <NavBar/>
@@ -19,18 +20,18 @@ const Blogs = () => {
 <br /> <br />
 
         <HoverCard 
-        imageUrl={Blog1}
-        title=" What is AR? "
+        imageUrl={Blog2}
+        title=" how doeas AR work? "
         time=""
-        tag="Yesterday"
+        tag="2 april"
       /> 
 
 <br /> <br />
   <HoverCard 
-        imageUrl={Blog1}
-        title=" What is AR? "
+        imageUrl={Blog3}
+        title=" What we all open AR together? "
         time=""
-        tag="Yesterday"
+        tag="1 april"
       /> 
 
 <Footer/>
